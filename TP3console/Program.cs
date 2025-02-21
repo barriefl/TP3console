dd0a16e5b98e8cfa1dc4ac809d421c79159a03d5
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-02-21 09:59:54 +0100

    Entity Framework

--- TP3console/Program.cs
+++ TP3console/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace TP3console
+﻿using TP3console.Models.EntityFramework;
+
+namespace TP3console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using (var ctx = new FilmsDbContext())
+            {
+                // Requête SELECT.
+                Film titanic = ctx.Films.First(f => f.Nom.Contains("Titanic"));
+
+                // Modification de l'entité (dans le contexte seulement).
+                titanic.Description = "Un bateau échoué. Date : " + DateTime.Now;
+
+                // Sauvegarde du contexte => Application de la modification dans la BD.
+                int nbchanges = ctx.SaveChanges();
+
+                Console.WriteLine("Nombre d'enregistrements modifiés ou ajoutés : " + nbchanges);
+            }
         }
     }
 }
